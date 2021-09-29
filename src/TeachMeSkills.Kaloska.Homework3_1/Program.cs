79d9e69efd981019a1236b3367c64ebb2a1c849f
GoduNull <61514386+GoduNull@users.noreply.github.com>
2021-09-30 01:01:32 +0300

    feat: add class WeekDayHandler

--- src/TeachMeSkills.Kaloska.Homework3_1/Program.cs
+++ src/TeachMeSkills.Kaloska.Homework3_1/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace TeachMeSkills.Kaloska.Homework3_1
 {
+
+    class WeekDayHandler
+    {
+
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
