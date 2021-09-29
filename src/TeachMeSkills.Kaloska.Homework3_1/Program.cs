74f71d2d7ccd0e86dc08120a190cfae85244c245
GoduNull <61514386+GoduNull@users.noreply.github.com>
2021-09-30 01:09:08 +0300

    feat: add static method DayOfWeek

--- src/TeachMeSkills.Kaloska.Homework3_1/Program.cs
+++ src/TeachMeSkills.Kaloska.Homework3_1/Program.cs
@@ -8,12 +8,29 @@ namespace TeachMeSkills.Kaloska.Homework3_1
     {
 
 
+        public static void DayOfWeek(string day)
+        {
+            string reg = @"\d{2}-[0-1]{1}[0-9]{1}-\d{4}"; 
+            if (Regex.IsMatch(day, reg, RegexOptions.IgnoreCase))
+            {
+                DateTime.TryParse(day, out DateTime date);
+                Console.WriteLine("День недели:");
+                Console.WriteLine(date.DayOfWeek); ;
+            }
+            else
+            {
+                Console.WriteLine("Некорректная дата");
+            }
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Введите дату ");
+            string dateInput = Console.ReadLine();
+            WeekDayHandler.DayOfWeek(dateInput);
+            Console.ReadKey();
         }
     }
 }
