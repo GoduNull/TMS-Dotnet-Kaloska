0ce31d62dd80510214af8105829b7fef77d2ee6a
GoduNull <61514386+GoduNull@users.noreply.github.com>
2021-09-30 13:23:11 +0300

    refactor: improvement of method DayOfWeek

--- src/TeachMeSkills.Kaloska.Homework3_1/Program.cs
+++ src/TeachMeSkills.Kaloska.Homework3_1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace TeachMeSkills.Kaloska.Homework3_1
@@ -6,21 +7,23 @@ namespace TeachMeSkills.Kaloska.Homework3_1
 
     class WeekDayHandler
     {
-
-
-        public static void DayOfWeek(string day)
+        public static DateTime DayOfWeek(string day)
         {
-            string reg = @"\d{2}-[0-1]{1}[0-9]{1}-\d{4}"; 
+            DateTime date = new DateTime();
+            string reg = @"\d{2}-[0-1]{1}[0-9]{1}-\d{4}";
             if (Regex.IsMatch(day, reg, RegexOptions.IgnoreCase))
             {
-                DateTime.TryParse(day, out DateTime date);
+                DateTime.TryParse(day, out DateTime dates);
                 Console.WriteLine("День недели:");
-                Console.WriteLine(date.DayOfWeek); ;
+                Console.WriteLine(dates.DayOfWeek);
+                date = dates;
             }
             else
             {
                 Console.WriteLine("Некорректная дата");
             }
+            return date;
+            
         }
     }
     class Program
@@ -29,7 +32,7 @@ namespace TeachMeSkills.Kaloska.Homework3_1
         {
             Console.WriteLine("Введите дату ");
             string dateInput = Console.ReadLine();
-            WeekDayHandler.DayOfWeek(dateInput);
+            //WeekDayHandler.AllDayofweek(WeekDayHandler.DayOfWeek(dateInput));
             Console.ReadKey();
         }
     }
