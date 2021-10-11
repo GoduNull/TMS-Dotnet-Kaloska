da3e614d9d68afda40e0fb88fd52bee3ec2977dd
Dzmitry Kaloska <61514386+GoduNull@users.noreply.github.com>
2021-10-11 15:37:36 +0300

    feat: creating a constructor and method Getinfo to display the object

--- src/TeachMeSkills.Kaloska.Homework4/Diary.cs
+++ src/TeachMeSkills.Kaloska.Homework4/Diary.cs
@@ -8,6 +8,24 @@ namespace TeachMeSkills.Kaloska.Homework4
 {
     class Diary
     {
-
+        private readonly DateTime _dateTimeCreate;
+        public DateTime dataTimeTask;
+        public string Name { get; private set; }
+        public string Task { get; private set; }
+        public Diary(DateTime dateTime, string name, string task)
+        {
+            this._dateTimeCreate = DateTime.Now;
+            this.dataTimeTask = dateTime;
+            this.Name = name;
+            this.Task = task;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Название задачи: {Name}");
+            Console.WriteLine($"Дата создания: {_dateTimeCreate}");
+            Console.WriteLine($"Дата задачи: {_dateTimeCreate}");
+            Console.WriteLine($"Описание задачи: {Task}");
+        }
     }
 }
+
