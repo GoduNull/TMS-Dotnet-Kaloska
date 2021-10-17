5614bf130151fc335ca98dbe2ada5ecf3256ccfd
Dzmitry Kaloska <61514386+GoduNull@users.noreply.github.com>
2021-10-17 23:57:03 +0300

    feat: create abstaract class AnimalBase

---
+++ src/TeachMeSkills.Kaloska.Homework5/Models/AnimalBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeachMeSkills.Kaloska.Homework5.Interfaces;
+
+namespace TeachMeSkills.Kaloska.Homework5.Models
+{
+    public abstract class AnimalBase : ISay
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public override string ToString()
+        {
+            return $"Name: {this.Name} Age: {this.Age}";
+        }
+        public virtual void Say()
+        {
+            Console.WriteLine("Who am I");
+        }
+    }
+}
