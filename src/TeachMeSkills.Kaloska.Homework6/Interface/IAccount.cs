a75740eaddb27b3df9dc64bb596712f43234e97c
Dzmitry Kaloska <61514386+GoduNull@users.noreply.github.com>
2021-10-20 09:40:49 +0300

    feat: Creating an IAccount interface with methods Put,PutOnThePhone,Withdraw

---
+++ src/TeachMeSkills.Kaloska.Homework6/Interface/IAccount.cs
@@ -0,0 +1,11 @@
+﻿namespace TeachMeSkills.Kaloska.Homework6.Interface
+{
+    internal interface IAccount
+    {
+        void Put(decimal sum);
+
+        void PutOnThePhone(decimal sum, string phone);
+
+        void Withdraw(decimal sum);
+    }
+}
