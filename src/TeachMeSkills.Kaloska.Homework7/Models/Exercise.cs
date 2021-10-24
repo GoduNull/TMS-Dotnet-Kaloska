40ccb9d4623ce58c37e7fa65bce900c21483dd7d
Dzmitry Kaloska <61514386+GoduNull@users.noreply.github.com>
2021-10-24 20:01:15 +0300

    feat: added Models

---
+++ src/TeachMeSkills.Kaloska.Homework7/Models/Exercise.cs
@@ -0,0 +1,7 @@
+﻿namespace TeachMeSkills.Kaloska.Homework7.Models
+{
+    public class Exercise : Activity<double>
+    {
+        public int Count { get; set; }
+    }
+}
---
+++ src/TeachMeSkills.Kaloska.Homework7/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using TeachMeSkills.Kaloska.Homework7.Enums;
+
+namespace TeachMeSkills.Kaloska.Homework7.Models
+{
+    public class User
+    {
+        public User()
+        {
+
+        }
+        public User(string name, double weight, int height, int age, Gender gender)
+        {
+            this.FullName = name;
+            this.Weight = weight;
+            this.Height = height;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public string FullName { get; set; }
+
+        public double Weight { get; set; }
+
+        public int Height { get; set; }
+
+        public int Age { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public List<Run> Runs { get; set; } = new List<Run>();
+
+        public List<Exercise> Exercises { get; set; } = new List<Exercise>();
+    }
+}
