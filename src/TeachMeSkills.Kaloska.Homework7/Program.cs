5b56e5b7bab6394ad041e32f8fab6baf8a6fc894
Dzmitry Kaloska <61514386+GoduNull@users.noreply.github.com>
2021-10-24 20:09:01 +0300

    feat: added a service for calculating and saving statistics and a service for the application functionality

--- src/TeachMeSkills.Kaloska.Homework7/Program.cs
+++ src/TeachMeSkills.Kaloska.Homework7/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Threading;
+using TeachMeSkills.Kaloska.Homework7.Enums;
+using TeachMeSkills.Kaloska.Homework7.Interfaces;
+using TeachMeSkills.Kaloska.Homework7.Manager;
+using TeachMeSkills.Kaloska.Homework7.Models;
+using TeachMeSkills.Kaloska.Homework7.Services;
 
 namespace TeachMeSkills.Kaloska.Homework7
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            ActivityManager.Start();
+            Console.ReadKey();
+        }  
     }
 }
---
+++ src/TeachMeSkills.Kaloska.Homework7/Services/FunctionalityService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using TeachMeSkills.Kaloska.Homework7.Interfaces;
+using TeachMeSkills.Kaloska.Homework7.Models;
+
+namespace TeachMeSkills.Kaloska.Homework7.Services
+{
+    internal class FunctionalityService : IFunctionalityService
+    {
+        public event Action<string, DateTime> MyHandler;
+
+        public DateTime Execute(string message)
+        {
+            if (message != null)
+            {
+                var currentTime = DateTime.Now;
+                MyHandler?.Invoke(message, currentTime);
+                return currentTime;
+            }
+            else
+            {
+                throw new NullReferenceException($"message пуст");
+            }
+        }
+
+        public void ShowExercises(User user)
+        {
+            foreach (var run in user.Exercises)
+            {
+                Console.WriteLine($"{run.Name}, {run.End - run.Start}");
+            }
+        }
+
+        public void ShowAll(User user)
+        {
+            var activities = user.Runs
+               .Select(x => new
+               {
+                   x.Name,
+                   Data = $"{x.Data} - {x.Speed}",
+                   Date = x.Start,
+               })
+               .Concat(user.Exercises
+                   .Select(x => new
+                   {
+                       x.Name,
+                       Data = $"{x.Data} - {x.Count}",
+                       Date = x.Start,
+                   }))
+               .OrderBy(x => x.Date);
+
+            foreach (var activity in activities)
+            {
+                Console.WriteLine($"{activity.Name}, {activity.Date}: {activity.Data}");
+            }
+        }
+
+        public void ShowRuns(User user)
+        {
+            foreach (var run in user.Runs)
+            {
+                Console.WriteLine($"{run.Name}, {run.End - run.Start}");
+            }
+        }
+    }
+}
