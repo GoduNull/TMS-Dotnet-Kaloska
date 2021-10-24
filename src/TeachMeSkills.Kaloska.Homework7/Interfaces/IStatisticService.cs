ae5cd8ddc5d537d3e9e00d31ff74d2ca9fda80a8
Dzmitry Kaloska <61514386+GoduNull@users.noreply.github.com>
2021-10-24 20:01:38 +0300

    feat: added Interfaces

---
+++ src/TeachMeSkills.Kaloska.Homework7/Interfaces/IStatisticService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using TeachMeSkills.Kaloska.Homework7.Models;
+
+namespace TeachMeSkills.Kaloska.Homework7.Interfaces
+{
+    public interface IStatisticService
+    {
+        Statistic Get(User user, IEnumerable<int> data);
+    }
+}
